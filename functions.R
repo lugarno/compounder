4f03e26e836949ecb89391dbd8015bfc001045bd
lugarno <109272584+lugarno@users.noreply.github.com>
2022-07-18 13:35:31 +1000

    Corrected excel interaction

--- functions.R
+++ functions.R
@@ -13,7 +13,7 @@ GetCapIQ <- function(formula, close = FALSE) {
   
   # specify cells
   cell_range <- paste0("A1:A", length(formulas))
-  cells <- book$Worksheets("sheet1")$Range("A1:A2")
+  cells <- book$Worksheets("sheet1")$Range(cell_range)
   
   # write formulas
   cells[["FormulaArray"]] <- asCOMArray(formulas)
@@ -23,12 +23,15 @@ GetCapIQ <- function(formula, close = FALSE) {
   ex$Run("ThisWorkbook.RefreshCapIQ")
   
   # get results
-  Sys.sleep(10)
+  Sys.sleep(15)
   result <- unlist(cells[["Value"]])
   
   # close?
   if(close) {
+    book$close(FALSE)
+    Sys.sleep(1)
     ex$Quit()
+    Sys.sleep(1)
     try(system("Taskkill /IM Excel.exe /F"), silent = TRUE)
   }
   
@@ -39,9 +42,18 @@ GetCapIQ <- function(formula, close = FALSE) {
 
 if(FALSE){
   
-  formulas <- c('=@CIQ("NasdaqGS:GOOGL", "IQ_LASTSALEPRICE", "2021-01-02", "LOCAL")',
-                '=@CIQ("NasdaqGS:GOOGL", "IQ_LASTSALEPRICE", "2021-01-10", "LOCAL")')
+  library(RDCOMClient)
   
   
+  formulas <- c('=@CIQ("NasdaqGS:GOOGL", "IQ_LASTSALEPRICE", "2021-01-02", "LOCAL")')
+  GetCapIQ(formulas, close = TRUE)
+                
+  formulas <- c('=@CIQ("NasdaqGS:GOOGL", "IQ_LASTSALEPRICE", "2021-02-02", "LOCAL")',
+                '=@CIQ("NasdaqGS:GOOGL", "IQ_LASTSALEPRICE", "2021-03-10", "LOCAL")')
+  GetCapIQ(formulas, close = TRUE)
+  
+  formulas <- c('=@CIQ("NasdaqGS:GOOGL", "IQ_LASTSALEPRICE", "2021-04-02", "LOCAL")',
+                '=@CIQ("NasdaqGS:GOOGL", "IQ_LASTSALEPRICE", "2021-05-10", "LOCAL")',
+                '=@CIQ("NasdaqGS:GOOGL", "IQ_LASTSALEPRICE", "2021-06-10", "LOCAL")')
   GetCapIQ(formulas, close = TRUE)
 }
